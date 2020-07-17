f2e7786632d5585888598272e35a1e37fdd5da73
SayemKaderChowdhury <sayem.kader.chowdhury@g.bracu.ac.bd>
2020-07-17 11:26:23 +0600

    Added relationship with the users

--- profiles/models.py
+++ profiles/models.py
@@ -23,7 +23,7 @@ class Profile(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user.username}-{self.created}"
+        return f"{self.user.username}-{self.created.strftime('%d-%m-%Y')}"
 
 
     def save(self, *args, **kwargs):
@@ -39,3 +39,18 @@ class Profile(models.Model):
         self.slug = to_slug
         super().save(*args, **kwargs)
 
+
+STATUS_CHOICES = (
+    ('send', 'send'),
+    ('accepted', 'accepted')
+)
+
+class Relationship(models.Model):
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
+    reciever = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='reciever')
+    status = models.CharField(max_length=8, choices=STATUS_CHOICES)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.sender}-{self.reciever}-{self.status}"
